4bc8f9764530aff029a8ab4a2ab38a3d1d2edfd8
Mike Watkins <github@mikewatkins.ca>
2022-04-28 10:21:50 -0700

    rename to avoid update issues

--- void-setup.sh
+++ void-setup.sh
@@ -1,55 +1,60 @@
 #!/bin/sh
 # This script copies patch files for dwm/dmenu/st to ~/src/void-packages/srcpkgs/*/patches
+# To setup void-packages:
+# 	mkdir -p ~/src && cd ~/src
+# 	git clone https://github.com/void-linux/void-packages
+#   cd void-packages
+# 	# Edit etc/defaults.conf appropriately:
+#   # one of the Tier-1 US mirrors:
+#   XBPS_MIRROR=https://repo-us.voidlinux.org/current
+#   echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+#   ./xbps-src binary-bootstrap
+
 SUCKLESS_PATCHES="$HOME/src/suckless-patches"
 VOID_PACKAGES="$HOME/src/void-packages"
 
 if [ -d $VOID_PACKAGES ]; then
 	echo "Installing patch files"
-	for ipkg in dwm dmenu st slstatus libXft; do
+	for ipkg in libXft dwm dmenu st slstatus; do
 		mkdir -p $VOID_PACKAGES/srcpkgs/$ipkg/patches
-		echo "copying $SUCKLESS_PATCHES/$ipkg/* to $VOID_PACKAGES/srcpkgs/$ipkg/patches"
-		cp $SUCKLESS_PATCHES/$ipkg/* $VOID_PACKAGES/srcpkgs/$ipkg/patches
+		echo "copying $SUCKLESS_PATCHES/$ipkg/*.diff to $VOID_PACKAGES/srcpkgs/$ipkg/patches"
+		cp $SUCKLESS_PATCHES/$ipkg/*.diff $VOID_PACKAGES/srcpkgs/$ipkg/patches
 	done
+else
+	echo "
+**Can't proceed**
+$HOME/src/void-packages does not exist! See the Quick Start at https://github.com/void-linux/void-packages."
 
-echo "
+fi
+
+while true; do
+	read -p "(re)Install patched applications [Yy]es,[Nn]o " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "
+
 Patch files copied. To build the packages:
 
 	cd $VOID_PACKAGES
-	# pkgname = dwm or st or dmenu
-	./xbps-src pkg pkgname
-	# -f to force reinstallation 
+	./xbps-src pkg <pkgname>
+	# install xtools, very handy
 	xi -f pkgname
+	# or, if xtools not installed
+	# ./xbps-install -f --repository hostdir/binpkgs <pkgname>
+"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "Building applications..."
+cd $VOID_PACKAGES
+for ipkg in libXft libXft-devel dwm dmenu st slstatus; do
+	echo "Building $ipkg"
+	./xbps-src pkg $ipkg
+	sudo xbps-install -f --repository hostdir/binpkgs $ipkg
+done
+
+echo "
+-------------------------------------------------
+Build and install complete.
 "
-else
-
-	echo "
-**Warning**
-$HOME/src/void-packages does not exist! To setup void-packages:
-
-	mkdir -p ~/src && cd ~/src
-	git clone https://github.com/void-linux/void-packages
-    cd void-packages
-
-Configure your preferred mirror first, if the default server in Germany or
-Finland is far from you. For example, to set the mirror xbps-src will use for
-bootstrap and building packages, edit etc/defaults.conf (etc within the
-void-packages directory):
-
-    # one of the Tier-1 US mirrors:
-    XBPS_MIRROR=https://repo-us.voidlinux.org/current
-
-A list of mirrors can be found at <https://docs.voidlinux.org/xbps/repositories/mirrors/index.html>.
-
-Finish preparing the void-packages local repository and enable restricted
-packages in order to add applications like Zoom or Discord. 
-
-Note: If running Void Linux musl variant (rather than glibc), proprietary
-packages like Zoom or Google Chrome will need to be installed and run using
-other methods such as a glibc chroot or perhaps more conviently for most
-packages, flatpak.
-
-    ./xbps-src binary-bootstrap
-    echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
-"
-fi
-
